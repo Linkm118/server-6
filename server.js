3e32d01163986d80106721c2c92d1dec8acb7b3c
Linkm118 <206734997+Linkm118@users.noreply.github.com>
2025-11-07 12:14:29 +0900

    削除まで

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-// 今回はクライアントサイドもルーティングもないので何もimportしない
+const kv = await Deno.openKv();
+
+await kv.set(['pokemon', 'ブラッキー'], { type: '悪', level: 35 });
+let pkmn = await kv.get(['pokemon', 'ブラッキー']);
+// console.log(pkmn.key, pkmn.value);
+
+// レコードの削除
+await kv.delete(['pokemon', 'ブラッキー']);
+
+// 確認
+pkmn = await kv.get(['pokemon', 'ブラッキー']);
+console.log(pkmn.value); // → null
